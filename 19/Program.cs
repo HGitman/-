1a8ebbcbb0bc08bc045820e5e854a1367bfb6e90
Bairam Mukhiev <Muhiyev@bk.ru>
2022-07-05 21:40:12 +0300

    all

--- 19/Program.cs
+++ 19/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+Console.WriteLine("Введите пятизначное число:");
+string chislo = Console.ReadLine();
+
+if (chislo[0] == chislo[4] && chislo[1] == chislo[3])
+{ System.Console.WriteLine("yes polindrom"); }
+else
+{ System.Console.WriteLine("no not polindrom"); }
--- 21/Program.cs
+++ 21/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход координаты
+//двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+System.Console.WriteLine("add a(,)");
+string A = Console.ReadLine();
+var a1 = A.Split(',').Select(int.Parse).ToArray();
+
+System.Console.WriteLine("add B (,)");
+string B = Console.ReadLine();
+var b1 = B.Split(',').Select(int.Parse).ToArray();
+
+double d = Math.Sqrt(Math.Pow(b1[0]-a1[0],2) + Math.Pow(b1[1]-a1[1],2) + Math.Pow(b1[2]-a1[2],2));
+System.Console.WriteLine($"Расстояние между точками: {d}");
+
